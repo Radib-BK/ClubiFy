6e169e79063713faf36942fee8c1c2388052d6a2
Radib Bin Kabir <radib.kabir@dsinnovators.com>
2025-12-30 13:07:13 +0600

    user profile page added

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('signup/', views.SignUpView.as_view(), name='signup'),
     path('login/', views.CustomLoginView.as_view(), name='login'),
     path('logout/', views.CustomLogoutView.as_view(), name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
-
