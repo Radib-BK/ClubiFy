5738c92368203c472a7766fd9e7f1117a4cd32cb
Radib Bin Kabir <84987225+Radib-BK@users.noreply.github.com>
2026-01-08 01:36:08 +0600

    Better UI the Post List pages

--- clubify/urls.py
+++ clubify/urls.py
@@ -1,12 +1,12 @@
 """clubify URL Configuration"""
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import RedirectView
+from clubs.views import ClubListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
-    path('', RedirectView.as_view(url='/clubs/', permanent=False), name='home'),
+    path('', ClubListView.as_view(), name='home'),
     path('clubs/', include('clubs.urls')),
     path('', include('memberships.urls')),
     path('', include('posts.urls')),
